3b8afb204646fa994115e1cb67211210e53eb9ce
Divyanshu Mishra <divyanshu.mishra.ai@gmail.com>
2026-02-07 00:31:36 +0530

    Add LeetCode 155 (Min Stack) Time Complexity: O(1) and Space Complexity: O(n).

---
+++ stack/min_stack.cpp
@@ -0,0 +1,41 @@
+// Problem: Min Stack
+// Platform: LeetCode 155
+// Approach: Make two stack one main and another minStack for storing minimum.
+// Time Complexity: O(1)
+// Space Complexity: O(n)
+
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> minSt;
+
+public:
+    MinStack() {
+    }  
+    
+    void push(int val) {
+        st.push(val);
+        if (minSt.empt() || val <= minSt.top()) {
+            minSt.push(val);
+        }
+        
+    }
+    
+    void pop() {
+        if (st.top() = minSt.top()) {
+            minSt.pop();
+        } 
+        st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+        
+    }
+    
+    int getMin() {
+        return minSt.top();
+        
+    }
+};
