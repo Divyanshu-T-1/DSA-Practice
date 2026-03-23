a12614dfa9fa7619b85e4810521eb22b26bf6b0e
Divyanshu Mishra <divyanshu.mishra.ai@gmail.com>
2026-03-24 00:30:52 +0530

    resolved merge conflict and cleaned reverse linked list

--- linked_list/reverse_link_list.cpp
+++ linked_list/reverse_link_list.cpp
@@ -1,11 +1,7 @@
 // Problem: Reverse Linked List, LeetCode 206
 // Approach: Two Pointer
-// Time Complexity: O(n) due to n element
-<<<<<<< HEAD
-// Space Complexity: O(1) because we use three pointers only which take constant space
-=======
-// Space Complexity: O(1) because we use three pointers that take constant space 
->>>>>>> 5e03241 (Add Linked Lsit Cycle, LeetCode 141, solved using Floyed's Cycle Detection in O(n) time and O(1) constant space complexity.)
+// Time Complexity: O(n) due to n elements
+// Space Complexity: O(1) because we use three pointers that take constant space
 
 class Solution {
 public:
@@ -13,14 +9,13 @@ public:
         ListNode* prev = NULL;
         ListNode* curr = head;
 
+        while(curr != NULL) {
+            ListNode* next = curr->next; // store next
+            curr->next = prev;           // reverse link
+            prev = curr;                 // move prev
+            curr = next;                 // move curr
+        }
 
-    while(curr != NULL) {
-        ListNode* next = curr->next; // store next
-        curr->next = prev;       // reverse link
-        prev = curr;             // move prev
-        curr = next;             // move curr
+        return prev;
     }
-
-    return prev;
-}
 };
