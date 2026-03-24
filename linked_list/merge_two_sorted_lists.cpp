fe97adf05fbd57afaeedc4b57e9a8085573da49e
Divyanshu Mishra <divyanshu.mishra.ai@gmail.com>
2026-03-24 17:17:07 +0530

    Adding Merge Two Sorted Lists, LeetCode 21 in O(n + m) time complexity and O(1) constant space.

---
+++ linked_list/merge_two_sorted_lists.cpp
@@ -0,0 +1,43 @@
+// Problem: Merge Two Sorted Lists, LeetCode 21
+// Approach: Create a dummy node, travese both lists, compare both and add smaller node
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(l1 != NULL && l2 != NULL) {
+
+            if(l1->val <= l2->val) {
+                temp->next = l1;
+                l1 = l1->next;
+            } else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+
+        if(l1 != NULL) {
+            temp->next = l1;
+        } else {
+            temp->next = l2;
+        }
+
+    return dummy->next;    
+}
+};
